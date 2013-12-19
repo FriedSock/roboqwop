b4aa9320f9c7d7f6db044e0cb31417d08739fac1
Jack Bracewell <jbtwentythree@gmail.com>
2013-12-19 18:44:34 +0000

    Initial commit

---
+++ test.rb
@@ -0,0 +1,36 @@
+require 'selenium-webdriver'
+require 'debugger'
+require 'tesseract'
+
+browser = Selenium::WebDriver.for :chrome
+
+#Unfortunately, the swf has dependencies.
+browser.get 'http://www.foddy.net/Athletics.html'
+
+# Click to start the game
+
+e = Tesseract::Engine.new {|e|
+    e.language  = :eng
+    e.blacklist = '|'
+}
+
+root = browser.find_element id: 'test'
+root.click
+
+(1..20).each do |i|
+  root.send_keys 'ooooooooooooo'
+end
+text = 'screenshot.png'
+browser.save_screenshot text
+raw = e.text_for(text)
+
+distance = raw.match(/(\S*) metres/)[1]
+puts "Distance travelled: " + distance
+
+if raw.include? 'showing real courage'
+  puts "Roboqwop fell over"
+else
+  puts "Roboqwop was still running"
+end
+
+browser.quit
